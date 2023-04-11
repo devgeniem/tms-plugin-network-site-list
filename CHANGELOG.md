0e7d4670c7435b9d9f26defecffb10217fa60e09
Timo Haapala <timo.haapala@geniem.com>
2023-04-11 11:14:58 +0300

    1.1.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,8 @@ and this project adheres to [Semantic Versioning][semver].
 
 ## Unreleased
 
+## [1.1.0] - 2023-04-11
+
 - PIEN-8157: PHP 8.1
   
 ## [1.0.0] - 2022-08-23
--- plugin.php
+++ plugin.php
@@ -3,8 +3,8 @@
  * Plugin Name: TMS Network Site List
  * Plugin URI: https://github.com/devgeniem/tms-plugin-network-site-list
  * Description: Provides a REST endpoint that lists the sites in a network installation.
- * Version: 0.1.0
- * Requires PHP: 7.4
+ * Version: 1.1.0
+ * Requires PHP: 8.1
  * Author: Geniem Oy
  * Author URI: https://geniem.com
  * License: GPL v3 or later
